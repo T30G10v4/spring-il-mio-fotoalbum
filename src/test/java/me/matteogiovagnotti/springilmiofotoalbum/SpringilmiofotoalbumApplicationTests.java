dcc591d043c3e9b1febd444e7d3148ecb35631af
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-04-13 11:03:25 +0200

    Initial commit

---
+++ src/test/java/me/matteogiovagnotti/springilmiofotoalbum/SpringilmiofotoalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package me.matteogiovagnotti.springilmiofotoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringilmiofotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
